9be81c34308921ad83babfe3fed275de993fa799
JorgeGonzalezPascual <61434203+JorgeGonzalezPascual@users.noreply.github.com>
2021-01-23 20:17:10 +0100

    Update malloc

--- av3.c
+++ av3.c
@@ -97,12 +97,11 @@ struct my_stack *init_stack(char *file)
     // Si no esta creada la pila la creamos
     else
     {
-        stack = my_stack_init(sizeof(int*));
+        stack = my_stack_init(sizeof(int *));
         printf("Stack->size: %ld\n", sizeof(int));
         filling();
+        printf("Longitud final de la nueva pila: %d\n", my_stack_len(stack));
     }
-
-    printf("Longitud final de la nueva pila: %d\n", my_stack_len(stack));
 
     return stack;
 }
@@ -115,12 +114,24 @@ void filling()
 {
     struct my_stack *auxStack = my_stack_init(stack->size);
     int filled = 0;
+    int *data = malloc(sizeof(int));
+    if (data == NULL)
+    {
+        fprintf(stderr, "No hay espacio en memoria dinámica disponible en este momento.\n");
+        return;
+    }
 
     printf("Contenido inicial de la pila:\n");
-
+    //Leemos el contenido de la pila
     while (my_stack_len(stack) != 0)
     {
-        int *data = malloc(sizeof(int));
+        data = malloc(sizeof(int));
+        if (data == NULL)
+        {
+            fprintf(stderr, "No hay espacio en memoria dinámica disponible en este momento.\n");
+            return;
+        }
+        
         data = my_stack_pop(stack);
         printf("%d\n", *data);
         my_stack_push(auxStack, data);
@@ -128,8 +139,14 @@ void filling()
     //Rellenamos en el caso de que falten
     while (my_stack_len(auxStack) < NUM_THREADS)
     {
+        data = malloc(sizeof(int));
+        if (data == NULL)
+        {
+            fprintf(stderr, "No hay espacio en memoria dinámica disponible en este momento.\n");
+            return;
+        }
+
         filled = 1;
-        int *data = malloc(sizeof(int));
         *data = 0;
         my_stack_push(auxStack, data);
     }
@@ -141,7 +158,13 @@ void filling()
     {
         while (my_stack_len(auxStack) != 0)
         {
-            int *data = malloc(sizeof(int));
+            data = malloc(sizeof(int));
+            if (data == NULL)
+            {
+                fprintf(stderr, "No hay espacio en memoria dinámica disponible en este momento.\n");
+                return;
+            }
+
             data = my_stack_pop(auxStack);
             printf("%d\n", *data);
             my_stack_push(stack, data);
